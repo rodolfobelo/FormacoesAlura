b3a32848803cd356423651610149946e93b93883
Rodolfo Freire Belo <rodolfo.belo@fametro.com.br>
2026-02-13 15:43:25 -0300

    add if para cair nas funcionalidades

--- PythonOO/SaborExpress.py
+++ PythonOO/SaborExpress.py
@@ -9,8 +9,15 @@ print('''
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+print('3. Ativar restaurante\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção:'))
+
+if opcao_escolhida == 1:
+    print(f'Você escolhei a opção, {opcao_escolhida}. Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print(f'Você escolhei a opção, {opcao_escolhida}. Listar restaurante')
+elif opcao_escolhida == 3:
+    print(f'Você escolhei a opção, {opcao_escolhida}. Ativar restaurante')
+else:
+    print(f'Encerrar programa')
