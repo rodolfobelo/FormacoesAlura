f7443cc7fb9144ae13d7261a5d2b6d7640a10e3e
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-02-18 12:30:27 -0300

    metodo limpar terminal

--- PythonOO/SaborExpress.py
+++ PythonOO/SaborExpress.py
@@ -17,22 +17,26 @@ def exibir_opcoes_app():
     print('4. Sair\n')
     
 def finalizar_app():
-    # os.system('clr') #Windows
-    os.system('clear') #mac
     print('Finalizando app!\n')
+
+def limpar_terminal():
+    os.system('cls') #Windows
+    # os.system('clear') #mac
 
 def escolher_opcao_app():
     opcao_escolhida = int(input('Escolha uma opção:'))
     if opcao_escolhida == 1:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Cadastrar restaurante')
+        print(f'A opção escolhida foi: {opcao_escolhida}. Cadastrar restaurante!\n')
     elif opcao_escolhida == 2:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Listar restaurante')
+        print(f'A opção escolhida foi: {opcao_escolhida}. Listar restaurante!\n')
     elif opcao_escolhida == 3:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante')
+        print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante!\n')
     else:
+        limpar_terminal()
         finalizar_app()
 
 def main():
+    limpar_terminal()
     exibir_nome_programa()
     exibir_opcoes_app()
     escolher_opcao_app()
