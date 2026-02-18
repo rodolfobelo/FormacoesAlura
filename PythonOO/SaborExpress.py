789ed0b160fc798d487543322229de7b55ae4eb9
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-02-17 22:06:08 -0300

    Aula 02

--- PythonOO/SaborExpress.py
+++ PythonOO/SaborExpress.py
@@ -1,4 +1,7 @@
-print('''
+import os
+
+def exibir_nome_programa():
+    print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -7,17 +10,32 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 ''')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante\n')
+def exibir_opcoes_app():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+    
+def finalizar_app():
+    # os.system('clr') #Windows
+    os.system('clear') #mac
+    print('Finalizando app!\n')
 
-opcao_escolhida = int(input('Escolha uma opção:'))
+def escolher_opcao_app():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+    if opcao_escolhida == 1:
+        print(f'A opção escolhida foi: {opcao_escolhida}. Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print(f'A opção escolhida foi: {opcao_escolhida}. Listar restaurante')
+    elif opcao_escolhida == 3:
+        print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante')
+    else:
+        finalizar_app()
 
-if opcao_escolhida == 1:
-    print(f'Você escolhei a opção, {opcao_escolhida}. Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print(f'Você escolhei a opção, {opcao_escolhida}. Listar restaurante')
-elif opcao_escolhida == 3:
-    print(f'Você escolhei a opção, {opcao_escolhida}. Ativar restaurante')
-else:
-    print(f'Encerrar programa')
+def main():
+    exibir_nome_programa()
+    exibir_opcoes_app()
+    escolher_opcao_app()
+
+if __name__ == '__main__':
+    main()
---
+++ PythonOO/Testes.py
@@ -0,0 +1,5 @@
+numero = int(input('Entre com um numero: '))
+if numero % 2 == 0:
+    print(f'Número {numero} é par.')
+else:
+    print(f'Número {numero} é impar')
