17ea33835d09ac633bbb5a4c7be64a052b3891cb
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-02-19 21:11:47 -0300

    SaborExpress - Cadastrar e LIstar restaurante

--- PythonOO/SaborExpress.py
+++ PythonOO/SaborExpress.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_programa():
     print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -23,17 +25,44 @@ def limpar_terminal():
     os.system('cls') #Windows
     # os.system('clear') #mac
 
+def opcao_invalida():
+    input('Opção inválida!\nDigite uma tecla para voltar ao menu principal.\n')
+    main()
+
+def cadastrar_novo_restaurante(opcao_escolhida):
+    limpar_terminal()
+    print(f'A opção escolhida foi {opcao_escolhida}. Cadastrar restaurante!\n')
+    nome_restaurante = input('Digite o nome do restaurante que você deseja cadastrar:\n')
+    restaurantes.append(nome_restaurante)
+    print(f'Seu restaurante {nome_restaurante}, foi cadastrado com sucesso.\n')
+    input('Digite um tecla para voltar ao menu principal.\n')
+    main()
+
+def listar_restaurante(opcao_escolhida):
+    limpar_terminal()
+    print(f'A opção escolhida foi {opcao_escolhida}. Listar restaurante!\nSegue lista de restaurante(s) cadastrado(s).')
+    for i, restaurante in enumerate(restaurantes):
+        print(f'{i}.{restaurante}')
+    input('Digite um tecla para voltar ao menu principal.\n')
+    main()
+
+
 def escolher_opcao_app():
-    opcao_escolhida = int(input('Escolha uma opção:'))
-    if opcao_escolhida == 1:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Cadastrar restaurante!\n')
-    elif opcao_escolhida == 2:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Listar restaurante!\n')
-    elif opcao_escolhida == 3:
-        print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante!\n')
-    else:
-        limpar_terminal()
-        finalizar_app()
+    try:
+        opcao_escolhida = int(input('Escolha uma opção:'))
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante(opcao_escolhida)
+        elif opcao_escolhida == 2:
+            listar_restaurante(opcao_escolhida)
+        elif opcao_escolhida == 3:
+            print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante!\n')
+        elif opcao_escolhida == 4:
+            limpar_terminal()
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
     limpar_terminal()
