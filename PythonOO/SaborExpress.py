cefc6b02052389d27a2d15ce6b910a60e0221946
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-03-09 22:22:22 -0300

    Alternar restaurante

--- PythonOO/SaborExpress.py
+++ PythonOO/SaborExpress.py
@@ -18,15 +18,15 @@ def exibir_nome_programa():
 def exibir_opcoes_app():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar restaurante')
     print('4. Sair\n')
     
 def finalizar_app():
     print('Finalizando app!\n')
 
 def limpar_terminal():
-    os.system('cls') #Windows
-    # os.system('clear') #mac
+    # os.system('cls') #Windows
+    os.system('clear') #mac
 
 def voltar_main():
     input('Digite um tecla para voltar ao menu principal.\n')
@@ -36,9 +36,9 @@ def opcao_invalida():
     input('Opção inválida!\nDigite uma tecla para voltar ao menu principal.\n')
     main()
 
-def cadastrar_novo_restaurante(opcao_escolhida):
+def cadastrar_novo_restaurante():
     limpar_terminal()
-    print(f'A opção escolhida foi {opcao_escolhida}. Cadastrar restaurante!\n')
+    print(f'A opção escolhida foi: Cadastrar restaurante!\n')
     nome_restaurante = input('Digite o nome do restaurante que você deseja cadastrar:\n')
     categoria_restaurante = input(f'Digite a categoria do restaurante {nome_restaurante}:\n')
     ativo_restaurante = False
@@ -47,34 +47,43 @@ def cadastrar_novo_restaurante(opcao_escolhida):
     print(f'Seu restaurante {nome_restaurante}, de categoria {categoria_restaurante} foi cadastrado com sucesso e se encontra desativado.\n')
     voltar_main()
 
-def listar_restaurante(opcao_escolhida):
+def listar_restaurante():
     limpar_terminal()
-    print(f'A opção escolhida foi {opcao_escolhida}. Listar restaurante!\nSegue lista de restaurante(s) cadastrado(s).')
+    print(f'A opção escolhida foi: Listar restaurante!\nSegue lista de restaurante(s) cadastrado(s).')
     for restaurante in restaurantes:
         nome_restaurante      = restaurante['nome']
         categoria_restaurante = restaurante['categoria']
         ativo_restaurante     = restaurante['ativo']
         if ativo_restaurante == True:
-            print(f'- {nome_restaurante} se encontra ativo!')
+            print(f'- {nome_restaurante} | ativo!')
         elif ativo_restaurante == False:
-            print(f'- {nome_restaurante} se encontra desativado!')
+            print(f'- {nome_restaurante} | desativado!')
     voltar_main()
 
-def alternar_estado_restaurante(opcao_escolhida):
+def alternar_estado_restaurante():
     limpar_terminal()
-    print(f'A opção escolhida foi: {opcao_escolhida}. Ativar restaurante!\n')
-
+    print(f'A opção escolhida foi: Alternar estado do restaurante!\n')
+    nome_restaurante = input('Qual restaurante você deseja alternar o estado:\n')
+    restaurante_encontrado = False
+    for restaurante in restaurantes:
+        if restaurante['nome'] == nome_restaurante:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {restaurante["nome"]} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {restaurante["nome"]} foi desativado com sucesso!'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print(f'Restaurante não encontrado')
     voltar_main()
 
 def escolher_opcao_app():
     try:
         opcao_escolhida = int(input('Escolha uma opção:'))
         if opcao_escolhida == 1:
-            cadastrar_novo_restaurante(opcao_escolhida)
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            listar_restaurante(opcao_escolhida)
+            listar_restaurante()
         elif opcao_escolhida == 3:
-            alternar_estado_restaurante(opcao_escolhida)
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             limpar_terminal()
             finalizar_app()
