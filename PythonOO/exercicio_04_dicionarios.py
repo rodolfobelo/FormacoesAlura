d3e040a59d09f0b4f08d216d5a56742cab418777
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-03-15 12:24:20 -0300

    Update exercicio_04_dicionarios.py

--- PythonOO/exercicio_04_dicionarios.py
+++ PythonOO/exercicio_04_dicionarios.py
@@ -9,11 +9,30 @@ pessoas = [{'nome':'Rodolfo', 'idade':39, 'cidade':'maranguape'},
 # 2 - Utilizando o dicionário criado no item 1:
 
 # Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
-for pessoa in pessoas:
-    print(f'{pessoa['nome']} | {pessoa['idade']} | {pessoa['cidade']}')
-nome = input('De quem você deseja atualizar a idade? ')
-idade = int(input('Entre com a idade atualizada: '))
-pessoas['nome':f'{nome}']
+def lista_pessoas():
+    print('Nome'.ljust(20), '| Idade '.ljust(12), '| Cidade')
+    for pessoa in pessoas:
+        nome = pessoa['nome']
+        idade = pessoa['idade']
+        cidade = pessoa['cidade']
+        print(f'{nome.ljust(20)} | {str(idade).ljust(10)} | {cidade}')
+
+def atualiza_idade(busca_nome, idade_atualizada):
+    for pessoa in pessoas:
+        if busca_nome == pessoa['nome']:
+            pessoa['idade'] = idade_atualizada
+    print(f'Idade atualizada para o {busca_nome}')
+    lista_pessoas()
+
+def main():
+    lista_pessoas()
+    busca_nome = input('De quem você deseja atualizar a idade? ')
+    idade_atualizada = int(input('Entre com a idade atualizada: '))
+    atualiza_idade(busca_nome, idade_atualizada)
+
+if __name__ == '__main__':
+    main()
+
 # Adicione um campo de profissão para essa pessoa;
 # Remova um item do dicionário.
 # 3 - Crie um dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.
