4fd76b9b1e211cf24804f6ac8b503998a8a954fc
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-04-05 21:03:27 -0300

    aula 02 - contrutor e metodo listar

--- PythonOO/Aplicando_OO/restaurante.py
+++ PythonOO/Aplicando_OO/restaurante.py
@@ -1,13 +1,21 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria, ativo):
         self.nome = nome
         self.categoria = categoria
         self.ativo = ativo
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 
-restaurante_nome = 'testeNome'
-restaurante_1_categoria = 'testeCategoria'
+restaurante_1 = Restaurante('testeNome1', 'testeCategoria1', False)
+restaurante_1 = Restaurante('testeNome2', 'testeCategoria2', True)
 
-restaurante_1 = Restaurante(restaurante_nome, restaurante_1_categoria, False)
-
-print(restaurante_1.nome)
+Restaurante.listar_restaurantes()
