1560c505da6c4e61f3eb726d944a5f906c05067a
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-04-05 21:06:59 -0300

    aula 02

--- PythonOO/Aplicando_OO/restaurante.py
+++ PythonOO/Aplicando_OO/restaurante.py
@@ -1,5 +1,4 @@
 class Restaurante:
-<<<<<<< HEAD
     restaurantes = []
 
     def __init__(self, nome, categoria, ativo):
@@ -14,22 +13,9 @@ class Restaurante:
     def listar_restaurantes():
         for restaurante in Restaurante.restaurantes:
             print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
-=======
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
->>>>>>> c659c34e207acf6e68d45fa5d551501eab917e0d
 
 
 restaurante_1 = Restaurante('testeNome1', 'testeCategoria1', False)
 restaurante_1 = Restaurante('testeNome2', 'testeCategoria2', True)
 
-<<<<<<< HEAD
 Restaurante.listar_restaurantes()
-=======
-restaurante_1 = Restaurante(restaurante_nome, restaurante_1_categoria)
-
-print(restaurante_1.nome)
-print(restaurante_1.categoria)
->>>>>>> c659c34e207acf6e68d45fa5d551501eab917e0d
