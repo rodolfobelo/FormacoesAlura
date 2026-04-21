af2385cc51450954da0faeaacc7f3a8b7cd80dec
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2026-04-21 14:28:49 -0300

    subatituição para arquivos do curso

--- PythonOO/Aplicando_OO_Sabor_Express/app.py
+++ PythonOO/Aplicando_OO_Sabor_Express/app.py
@@ -1,30 +1,14 @@
-from models.restaurante import Restaurante
-from models.cardapio.bebida import Bebida
-from models.cardapio.prato import Prato
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
-restaurante_coxinhas = Restaurante('Coxinhas Gourmet', 'Salgados', 1000)
-bebida_suco = Bebida('Suco de Melancia', 7.00, '500 ml')
-prato_pao_alho = Prato('Pao de alho', 7.00, 'Pão carioquinha com uma incrivel temperado de alho.')
-restaurante_coxinhas.add_no_cardapio(bebida_suco)
-restaurante_coxinhas.add_no_cardapio(prato_pao_alho)
-# Restaurante.altera_situacao(restaurante_coxinhas)
-
-# restaurante_docinhos = Restaurante('Docinhos da Vovó', 'Doces')
-
-# restaurante_pizza = Restaurante('Pizza do Dino', 'Massas')
-
-
-# restaurante_coxinhas.receber_avaliacao('Rodolfo', 8)
-# restaurante_coxinhas.receber_avaliacao('Yara', 10)
-# restaurante_docinhos.receber_avaliacao('Rodolfo', 9)
-# restaurante_docinhos.receber_avaliacao('Yara', 10)
-
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
     print(bebida_suco)
-    print(prato_pao_alho)
-    # pass
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
--- PythonOO/Aplicando_OO_Sabor_Express/models/cardapio/bebida.py
+++
@@ -1,9 +0,0 @@
-from models.cardapio.item_cardapio import ItemCardapio
-
-class Bebida(ItemCardapio):
-    def __init__(self, nome, preco, tamanho):
-        super().__init__(nome, preco)
-        self._tamanho = tamanho
-    
-    def __str__(self):
-        return f'*****__Bebida__*****\n{'Nome'.ljust(20)} | {'Preço'.ljust(20)} | {'Tamanho'}\n{len(f'{'Nome'.ljust(20)} | {'Preço'.ljust(20)} | {'Tamanho'}') * '*'}\n{self._nome.ljust(20)} | {str(self._preco).ljust(20)} | {self._tamanho}'
